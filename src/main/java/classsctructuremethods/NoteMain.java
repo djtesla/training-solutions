3c5867a8d99cc5974a2d9afcc3919bce0a05ef8e
djtesla <djtesla@gmailcom>
2020-10-31 16:09:54 +0100

    Note Main javítás ellenőrzés után

--- src/main/java/classsctructuremethods/NoteMain.java
+++ src/main/java/classsctructuremethods/NoteMain.java
@@ -1,11 +1,25 @@
 package classsctructuremethods;
 
+import java.util.Scanner;
+
 public class NoteMain {
     public static void main(String[] args) {
         Note note = new Note();
-        note.setName("Todos");
-        note.setTopic("Weekend");
-        note.setText("Learning");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Note neve:");
+        String name = scanner.nextLine();
+
+        System.out.println("Note témája:");
+        String topic = scanner.nextLine();
+
+        System.out.println("Note szövege:");
+        String text = scanner.nextLine();
+
+        note.setName(name);
+        note.setTopic(topic);
+        note.setText(text);
 
         System.out.println(note.getNoteText());
     }
