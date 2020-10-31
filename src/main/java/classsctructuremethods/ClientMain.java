b8256a4ff10c180983d20d0c18264b2d052f1b21
djtesla <djtesla@gmailcom>
2020-10-31 15:45:50 +0100

    Client és Client Main

---
+++ src/main/java/classsctructuremethods/ClientMain.java
@@ -0,0 +1,20 @@
+package classsctructuremethods;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.setAddress("1186 Budapest Goroszló utca 13.");
+        client.setName("Zámbó Ernő");
+        client.setYear(1982);
+
+        System.out.println(client.getName());
+        System.out.println(client.getAddress());
+        System.out.println(client.getYear());
+
+        client.migrate("2220 Horthy 3.");
+
+        System.out.println("Új lakcíme: " + client.getAddress());
+
+            }
+
+}
