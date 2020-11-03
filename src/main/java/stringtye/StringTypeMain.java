ea3f9d2bc3206210e01a79f32f251dda8d84e515
djtesla <djtesla@gmailcom>
2020-11-03 16:11:51 +0100

    StringTypeMain commit

---
+++ src/main/java/stringtye/StringTypeMain.java
@@ -0,0 +1,34 @@
+package stringtye;
+
+public class StringTypeMain {
+    public static void main(String[] args) {
+
+       /* String s1 ="";
+        String s2 ="Hello World";
+        String s3 = "Hello" + " World";
+        System.out.println(s2.equals(s3));
+
+        System.out.println(s3.indexOf("World"));
+        System.out.println(s3.indexOf("xxx"));
+        System.out.println(s3.length());
+        System.out.println(s3.substring(0,3));
+
+        System.out.println(s3);
+        */
+    String prefix = "Hello";
+    String name = "John Doe";
+    String message = prefix + name;
+        System.out.println(message);
+    message = message + 444;
+        System.out.println(message);
+
+        boolean b = message.equals("Hello John Doe444");
+        System.out.println(b);
+        String con  = "" + "";
+        System.out.println(con.length());
+
+        System.out.println("Abcde".length());
+        System.out.println("Abcde".substring(0,1) + "," + "Abcde".substring(2,3));
+        System.out.println("Abcde".substring(0,3));
+    }
+}
