ca081551451ef0b34cf6c8904b8c5b44496748fc
djtesla <djtesla@gmailcom>
2020-11-03 19:23:16 +0100

    Array commit

--- src/main/java/array/ArrayHandler.java
+++ src/main/java/array/ArrayHandler.java
@@ -3,23 +3,23 @@ package array;
 public class ArrayHandler {
 
     boolean contains(int[] source, int itemToFind) {
-        boolean valasz = false;
+
         for (int item: source) {
 
            if (item == itemToFind) {
-               valasz = true;
+               return true;
            }
         }
-        return valasz;
+        return false;
     }
 
     int find(int[] source, int itemToFind){
-    int valasz = -1 ;
+
         for (int i = 0; i < source.length; i++) {
             if (source[i] == itemToFind) {
-                valasz = i;
+                return i;
             }
         }
-        return valasz;
+        return -1;
     }
 }
