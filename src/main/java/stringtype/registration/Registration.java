b2fb7641b0df7c955442c3d882530ea4f8c5e2a0
djtesla <djtesla@gmailcom>
2020-11-03 16:11:22 +0100

    Registration és userValidator commit

---
+++ src/main/java/stringtype/registration/Registration.java
@@ -0,0 +1,25 @@
+package stringtype.registration;
+
+import java.util.Scanner;
+
+public class Registration {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        userValidator uservalidator = new userValidator();
+
+        System.out.println("Kérem a felhasználó nevet: ");
+        String name = scanner.nextLine();
+        System.out.println(uservalidator.isValidUsername(name) ? "Helyesen megadtad a felhasználónevet." : "Nem adtál meg felhasználónevet");
+
+        System.out.println("Kérem az emailt: ");
+        String email = scanner.nextLine();
+        uservalidator.isValidUsername(name);
+        System.out.println(uservalidator.isValidEmail(email) ? "Helyes email címet adtál meg" : "Nem adtál meg helyes email címet.");
+
+        System.out.println("Kérem a jelszót: ");
+        String password1 = scanner.nextLine();
+        System.out.println("Kérem a jelszót mégegyszer: ");
+        String password2 = scanner.nextLine();
+        System.out.println(uservalidator.isValidPassword(password1, password2) ? "A két jelszó megegyezik." : "Állj állj! A két jelszó nem egyezik meg, vagy túl rövid!");
+    }
+}
---
+++ src/main/java/stringtype/registration/userValidator.java
@@ -0,0 +1,26 @@
+package stringtype.registration;
+
+public class userValidator {
+
+    public boolean isValidUsername (String username) {
+        return username != null && !"".equals(username);
+    }
+
+    public boolean isValidPassword (String password1, String password2) {
+        return password1 != null
+                && password1.length() > 7
+                && password1.equals(password2);
+
+    }
+
+    public boolean isValidEmail (String email) {
+        int atIndex = email.indexOf("@");
+        String domain =email.substring((atIndex+1));
+        int dotIndex = email.indexOf(".");
+
+        return atIndex > -1
+                && dotIndex > 0 && dotIndex > atIndex
+                && dotIndex !=domain.length() - 1;
+
+    }
+}
