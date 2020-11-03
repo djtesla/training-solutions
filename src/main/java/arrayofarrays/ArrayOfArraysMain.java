40234dfa5deb1b4fcc98834148b3ad22988c23e1
djtesla <djtesla@gmailcom>
2020-11-03 21:11:33 +0100

    Arrays + CommandLineArg commit

---
+++ src/main/java/arrayofarrays/ArrayOfArraysMain.java
@@ -0,0 +1,19 @@
+package arrayofarrays;
+
+public class ArrayOfArraysMain<b> {
+
+    public static void main(String[] args) {
+
+
+        int[][] a = new int[3][];
+//int[][] b = new int [3][];
+
+        a[0] = new int[1];
+
+        int[][] b = {{1,2},{1,2,3}, {1,2,3,4}};
+
+    }
+
+}
+
+
---
+++ src/main/java/arrays/ArraysMain.java
@@ -0,0 +1,29 @@
+package arrays;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+
+public class ArraysMain {
+
+    public static void main(String[] args) {
+        int[] a1 = {3, 2, 1};
+        int[] a2 = {3, 2, 1};
+
+        int[][] b1 = {{1}, {1,2}, {1,2,3}};
+        int[][] b2 = {{1}, {1,2}, {1,2,3}};
+        //System.out.println(a1);
+        //System.out.println(Arrays.toString(a1));
+        //System.out.println(Arrays.deepToString(b1));
+        //System.out.println(Arrays.equals(a1, a2));
+        //System.out.println(Arrays.deepEquals(b1, b2));
+
+        Arrays.sort(a1);;
+        //System.out.println(Arrays.toString(a1));
+        System.out.println(Arrays.toString(Arrays.copyOfRange(a1, 1, 3)));
+
+
+    }
+
+}
+
