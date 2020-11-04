ece7d0c2233a641304652b696ade4d8c073f5dc7
djtesla <djtesla@gmailcom>
2020-11-04 13:17:14 +0100

    Gyak szerda commit

---
+++ src/main/java/week02/Product.java
@@ -0,0 +1,27 @@
+package week02;
+
+public class Product {
+    private String name;
+    private String code;
+
+    public Product(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+    public boolean areTheyEqual(Product p) {
+        return name == p.name;
+    }
+
+    public boolean areTheyEqualRestricted(Product p) {
+        return code.length() - p.code.length() <= 1 || code.length() - p.code.length() >= -1 ;
+    }
+}
+
---
+++ src/main/java/week02/ProductMain.java
@@ -0,0 +1,16 @@
+package week02;
+
+public class ProductMain {
+    public static void main(String[] args) {
+
+
+        Product product1 = new Product("kenyér", "A1100");
+        Product product2 = new Product("kalács", "A110");
+        Product product3 = new Product("kenyér", "A1100");
+
+        System.out.println(product1.areTheyEqual(product2));
+        System.out.println(product1.areTheyEqual(product3));
+        System.out.println(product1.areTheyEqual(product2));
+        System.out.println(product1.areTheyEqual(product3));
+    }
+}
