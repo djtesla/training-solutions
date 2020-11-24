79b94c53f6d0c72152fe4ff8db98c996f92dde06
djtesla <djtesla@gmailcom>
2020-11-24 13:20:56 +0100

    commit

---
+++ src/main/java/week05/ChangeLetter.java
@@ -0,0 +1,39 @@
+package week05;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ChangeLetter {
+
+    public static String changeVowels(String text) {
+
+        List<Character> englishVowels = Arrays.asList('a', 'e','i', 'o', 'u');
+
+
+
+        for(int i = 0; i < text.length(); i++) {
+            if (englishVowels.contains(text.charAt(i))) {
+
+                text = text.replace(text.charAt(i), '*');
+
+            }
+
+        }
+        return text;
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println(changeVowels("dghzhnfgeeerr3we"));
+
+    }
+
+
+
+    // A week05d02.ChangeLetter osztályban írj meg egy String changeVowels(String) metódust, mely kicseréli a paraméterként átadott szövegben
+    // az angol magánhangzókat csillag karakterre. Írj hozzá teszteseteket is!
+
+}
