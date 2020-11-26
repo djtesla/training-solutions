49ee571709f463fe966ca432028b6cc9bed65f28
djtesla <djtesla@gmailcom>
2020-11-26 15:57:55 +0100

    commit

--- src/main/java/week05/week05d04/Store.java
+++ src/main/java/week05/week05d04/Store.java
@@ -9,8 +9,16 @@ public class Store {
     private List<Product> products = new ArrayList<>();
 
 
-    public void addProduct(Product product) {
-        products.add((product));
+    public void addProduct(Product productToAdd) {
+
+        for (Product product : products) {
+
+            if (!product.getName().equals(productToAdd.getName()) &&
+                    !productToAdd.getLocalDate().isBefore(LocalDate.now()));
+            products.add((product));
+        }
+        System.out.println("Lejárt a termék vagy már szerepel a listában!");
+
     }
 
     public int getNumberOfExpired() {
