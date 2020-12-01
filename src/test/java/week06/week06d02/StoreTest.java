ac623a48d1cd1c54d3bc20f4a9531a5afc96164f
djtesla <djtesla@gmailcom>
2020-12-01 15:39:45 +0100

    commit

--- src/test/java/week06/week06d02/StoreTest.java
+++ src/test/java/week06/week06d02/StoreTest.java
@@ -9,8 +9,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class StoreTest {
 
     Store store = new Store(Arrays.asList(new Product("kenyér",Category.BAKEDGOODS, 590),
-            new Product("liszt",Category.DAIRY, 390),
-            new Product("tej",Category.FROZEN, 490),
+            new Product("túró",Category.DAIRY, 390),
+            new Product("mirelit borsó",Category.FROZEN, 490),
     new Product("kalács",Category.BAKEDGOODS, 790)));
 
     @Test
