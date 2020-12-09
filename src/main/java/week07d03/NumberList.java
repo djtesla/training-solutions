c0bbb138d480461270fe42b8de8c60834914331a
djtesla <djtesla@gmailcom>
2020-12-09 13:15:04 +0100

    commit

--- src/main/java/week07d03/NumberList.java
+++ src/main/java/week07d03/NumberList.java
@@ -6,7 +6,6 @@ public class NumberList {
 
     public boolean isIncreasing(List<Integer> numbers) {
 
-        int prevNum;
 
         for (int i = 0; i < numbers.size()-1; i++) {
             if (numbers.get(i + 1) < numbers.get(i)) {
