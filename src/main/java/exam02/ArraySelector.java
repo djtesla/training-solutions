01ac5ab558c76fa96c2e7e90024d063315f44bd4
djtesla <djtesla@gmailcom>
2021-01-06 18:32:12 +0100

    exam02 commit kezdés

---
+++ src/main/java/exam02/ArraySelector.java
@@ -0,0 +1,29 @@
+package exam02;
+
+public class ArraySelector {
+
+
+}
+
+
+
+/*
+Tömbök
+Írj egy olyan metódust az ArraySelector osztályban, mely egy kapott tömbből minden második elemet kiválogat,
+és egy szövegként összerak, szögletes zárójelek között!
+
+A páros megállapítása index szerint működik. Azaz az első, nulla indexű elem páros, a második, azaz egyes indexű
+elem páratlan, stb.
+
+Ha nulla elemű a tömb, akkor üres stringet adjon vissza!
+
+CV
+
+Egy önéletrajzot reprezentáló osztályt kell létrehozni, melyben van egy név, valamint skillek (hogy mihez ért az illető).
+A skill egy névvel és egy számmal (szint) rendelkezik, utóbbi 1 - 5 között.
+
+Az önéletrajz a Cv osztály pár metódussal, a skill a Skill osztály.
+
+A skillek megadása a nevével, valamint a szintjével történik, pl. programming (5).
+
+A findSkillLevelByName() a skill neve alapján visszaadja annak szintjét.*/
---
+++ src/test/java/exam02/ArraySelectorTest.java
@@ -0,0 +1,7 @@
+package exam02;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArraySelectorTest {
+
+}
