c280150acb6d98f762479ac57a8e30eb6b498338
djtesla <djtesla@gmailcom>
2021-01-22 15:44:35 +0100

    konzi feladat Index

---
+++ src/main/java/week12d05/Index.java
@@ -0,0 +1,36 @@
+package week12d05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Index {
+
+
+    public int getNumberOfWords() {
+
+        int sum = 0;
+        String word = "koronavírus";
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+                Index.class.getResourceAsStream("/index.html")))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains(word)) {
+                    sum+=1;
+                }
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+
+        return sum;
+
+    }
+}
+
+
+/*    Mai junior/mid feladat:
+        Töltsd le az index.hu nyitólapját böngészőből, ez egy html állomány! Másold be a projektedbe
+        (index.html) néven! Ez egy szöveges állomány. Olvasd be, és számold meg, hány sorban szerepel a
+        koronavírus szó! (edited) */
---
+++ src/test/java/week12d05/IndexTest.java
@@ -0,0 +1,15 @@
+package week12d05;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexTest {
+
+    @Test
+    public void getNumberOfWordsTest() {
+
+        System.out.println(new Index().getNumberOfWords());
+    }
+
+}
